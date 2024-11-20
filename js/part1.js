4cdf4332fa8738ba5a960bed9534788ca0ddef16
belladianamiller <isabelladianamiller@gmail.com>
2024-11-20 13:31:53 -0500

    Update part1.js

--- js/part1.js
+++ js/part1.js
@@ -4,19 +4,14 @@ document.getElementById("get-btn").addEventListener("click", async () => {
     // This function should send a GET request to the echo endpoint and output the result
     // The two input fields should be included in the request URL as **query parameters**
     try {
-        let name = document.getElementById('name');
-        let age = document.getElementById('age');
-
-        let query = new URLSearchParams({q: query, page: pageNum});
+        let name = document.getElementById('name').value;
+        let age = document.getElementById('age').value;
+        let query = new URLSearchParams({name, age});
         let url = 'https://echo.zuplo.io/api?' + query.toString();
-
         let response = await fetch(url);
+        console.log(response);
         let data = await response.json();
-
-        console.log(data);
-
-        let text = document.getElementById('output');
-        let formattedText = JSON.stringify(data, null, 2);
+        output.textContent = JSON.stringify(data, null, 2);
 
     } catch (error) {
         console.error("Error fetching data: " + error);
