b351e012555c652651283f5a93e395da4af583a5
belladianamiller <isabelladianamiller@gmail.com>
2024-11-20 13:13:54 -0500

    Update part1.js

--- js/part1.js
+++ js/part1.js
@@ -3,8 +3,16 @@ const output = document.getElementById("output");
 document.getElementById("get-btn").addEventListener("click", async () => {
     // This function should send a GET request to the echo endpoint and output the result
     // The two input fields should be included in the request URL as **query parameters**
-
-    // TODO
+    try {
+        let name = 0;
+        let age = 0;
+        let query = new URLSearchParams({q: query, page: pageNum});
+        let response = await fetch('https://echo.zuplo.io/api?' + query.toString());
+        let data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Error fetching data: " + error);
+    }
 });
 
 document.getElementById("post-json-btn").addEventListener("click", async () => {
