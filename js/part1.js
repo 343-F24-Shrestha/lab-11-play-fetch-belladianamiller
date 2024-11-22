d068591899e93e1d342cd0af6bdd2de70435dc94
belladianamiller <isabelladianamiller@gmail.com>
2024-11-22 15:23:22 -0500

    Finish part 1

--- js/part1.js
+++ js/part1.js
@@ -1,36 +1,62 @@
 const output = document.getElementById("output");
 
 document.getElementById("get-btn").addEventListener("click", async () => {
-    // This function should send a GET request to the echo endpoint and output the result
-    // The two input fields should be included in the request URL as **query parameters**
     try {
         let name = document.getElementById('name').value;
         let age = document.getElementById('age').value;
 
-        let query = new URLSearchParams({name, age});
+        let query = new URLSearchParams({ name, age });
         let url = 'https://echo.zuplo.io/api?' + query.toString();
 
         let response = await fetch(url);
         console.log(response);
-        
+
         let data = await response.json();
         output.textContent = JSON.stringify(data, null, 2);
 
     } catch (error) {
-        console.error("Error fetching data: " + error);
+        console.error("Error: " + error);
     }
 });
 
 document.getElementById("post-json-btn").addEventListener("click", async () => {
-    // This function should send a POST request to the echo endpoint with the input data as JSON
-    // The two input fields should be included in the request body as **JSON data**
+    try {
+        let name = document.getElementById('name').value;
+        let age = document.getElementById('age').value;
 
-    // TODO
+        let options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, age }),
+        };
+        let response = await fetch("https://echo.zuplo.io/api", options);
+        let data = await response.json();
+
+        output.textContent = JSON.stringify(data, null, 2);
+    } catch (error) {
+        output.textContent = "Error: " + error;
+    }
 });
 
 document.getElementById("post-form-btn").addEventListener("click", async () => {
-    // This function should send a POST request to the echo endpoint with the input data as form data
-    // The two input fields should be included in the request body as **url-encoded data**
+    try {
+        let name = document.getElementById('name').value;
+        let age = document.getElementById('age').value;
 
-    // TODO
+        let options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: new URLSearchParams({name, age}).toString(),
+        };
+        let response = await fetch("https://echo.zuplo.io/api", options);
+        let data = await response.json();
+
+        output.textContent = JSON.stringify(data, null, 2);
+    } catch (error) {
+        output.textContent = "Error: " + error;
+    }
 });
